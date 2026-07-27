e656137f746900a33289cb1e5920ec4a58762423
arupremraj@gmail.com <Shivani@2625>
2026-07-27 23:23:04 +0530

    redio buttons

--- tests/playActions.spec.ts
+++ tests/playActions.spec.ts
@@ -16,7 +16,7 @@ await page.waitForTimeout(10000);
 
 })
 
-test.only("checkBox Actions", async({page})=>{
+test("checkBox Actions", async({page})=>{
 await page.goto("https://testautomationpractice.blogspot.com/");
 const maleRadio:Locator=page.locator("#male")
 await expect(maleRadio).toBeVisible();
@@ -24,4 +24,45 @@ await expect(maleRadio).toBeEnabled();
 await maleRadio.check();
 expect(await maleRadio.isChecked()).toBe(true);
   await page.waitForTimeout(10000);
+})
+
+test.only("radio button",async({page})=>{
+await page.goto("https://testautomationpractice.blogspot.com/");
+const Days:string[]=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+const checkBoxes:Locator[]=Days.map(index=>page.getByLabel(index)); // this will return checkBoxes
+expect(checkBoxes.length).toBe(7);
+
+for(const checkBox of checkBoxes){
+  await checkBox.check();
+  await expect(checkBox).toBeChecked();
+}
+// unselect last three checkboxes
+
+for(const checkBox of checkBoxes.slice(-3)){
+  await checkBox.uncheck();
+  await expect(checkBox).not.toBeChecked();
+}
+await page.waitForTimeout(10000);
+// select the check boxes those are unselected and visevsersa
+
+for(const checkBox of checkBoxes)
+{
+if(await checkBox.isChecked()) { 
+await checkBox.uncheck()
+await expect(checkBox).not.toBeChecked();
+}
+else{
+await checkBox.check();
+await expect(checkBox).toBeChecked();
+}}
+await page.waitForTimeout(5000);
+
+// Random checkbox to select
+const indexes:number[]=[1,3,5];
+for(const i of indexes){
+await checkBoxes[i].check();
+await expect(checkBoxes[i]).toBeChecked();
+
+}
+
 })

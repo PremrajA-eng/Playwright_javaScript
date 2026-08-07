f62eb6f3dee6fca5b91c1d92136a80c4537eabf4
arupremraj@gmail.com <Shivani@2625>
2026-08-07 21:11:37 +0530

    Add pagination filtering and search tests

--- tests/paginationTable.spec.ts
+++ tests/paginationTable.spec.ts
@@ -24,3 +24,39 @@ console.log("*************")
 }
 
 })
+
+test("filter the rows", async({page})=>{
+    await page.goto("https://datatables.net/examples/core/basic_init/zero_configuration.html");
+    const dropdown:Locator= page.locator("#dt-length-0");
+   await dropdown.selectOption(({label:'25'}));
+
+   const rows=await page.locator("#example tbody tr").all(); // array of locators
+    expect(rows.length).toBe(25);
+
+   const rows2= page.locator("#example tbody tr")
+  await expect(rows2).toHaveCount(25)
+
+})
+
+// searching data from atable
+
+test.only("Seaching element in table", async({page})=>{
+    await page.goto("https://datatables.net/examples/core/basic_init/zero_configuration.html");
+   await page.getByRole('searchbox', { name: 'Search:' }).fill("Cedric Kelly");
+    const rows= await page.locator("#example tbody tr").all();
+   if(rows.length>1){
+    let matchfound=false;
+     for(let row of rows)
+        {
+         const rowstext=await row.innerText();
+         if(rowstext.includes('Cedric Kelly'))
+         {
+            matchfound=true;
+            break;
+         }
+        }
+
+   }
+
+
+})
